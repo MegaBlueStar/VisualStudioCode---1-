7b54ee65240f4f24383c6576b0ccc09054f7bd29
MegaBlueStar <89914365+MegaBlueStar@users.noreply.github.com>
2022-11-15 16:41:29 +0530

    Add files via upload

---
+++ interact.js
@@ -0,0 +1,14 @@
+var a = 10;
+var b = 5;
+var firstname = "Adrien";
+var lastname = "Agreste";
+
+console.log(firstname);
+console.log(lastname);
+console.log(firstname.concat(lastname));
+console.log(Math.max(a,b));
+console.log(Math.min(a,b));
+console.log(a * b);
+console.log(a ^ b);
+
+
